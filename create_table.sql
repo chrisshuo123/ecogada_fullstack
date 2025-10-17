31a5ccb11408aa9165573e7ff9bd3da2c494d8b7
chrisshuo123 <chr1z100@gmail.com>
2025-10-18 00:54:45 +0700

    first commit after create some new tables for profile and products

---
+++ create_table.sql
@@ -0,0 +1,65 @@
+use ecogada;
+
+/* ====================== */
+/* = 1 - Table Profile == */
+/* ====================== */
+
+/* 1. Create table user */
+CREATE TABLE user (
+    idUser int(10) primary key auto_increment,
+    tglRegistrasi timestamp not null default current_timestamp,
+    namaUser varchar(100) not null
+);
+
+select * from user;
+
+/* 2. Create table profil kerjasama */
+CREATE TABLE profil_kerjasama(
+    idProfilKerjasama int(10) primary key auto_increment,
+    tglInput timestamp not null default current_timestamp,
+    namaKerjasama varchar(100) not null
+);
+
+select * from profil_kerjasama;
+
+/* 3. Create table strategic_partnership (m:n) */
+CREATE TABLE strategic_partnership (
+    idStrategicPartnership int(10) primary key auto_increment,
+    tglInput timestamp not null default current_timestamp,
+    idUser_fk int(10),
+    idProfilKerjasama_fk int(10),
+    idProduk_fk int(10)
+);
+
+select * from strategic_partnership;
+
+/* ====================== */
+/* = 2 - Table Produk == */
+/* ====================== */
+
+/* Dibagian table ini lebih banyak relasi fk nya berhubungan
+produk banyak komponen pendukung seperti list brand,
+list kategori produk, kondisi (baru, bekas), PreOrder (iya
+/ tidak), status, dan pengiriman (yg ini m:n) */
+
+/* 1. Create table Produk */
+CREATE TABLE produk (
+    idProduk int(10) primary key auto_increment,
+    tglRegistrasi timestamp not null default current_timestamp,
+    idBrand_fk int(10) not null,
+    idKategoriProduk_fk int(10) not null,
+    deskripsi TEXT not null,
+    hargaAwal int(10) not null,
+    hargaAkhir int(10) not null,
+    stok int(10),
+    sku varchar(100),
+    idKondisi_fk int(10) not null,
+    berat double(10, 2) not null,
+    uk_panjang double(10, 2),
+    uk_lebar double(10, 2),
+    uk_tinggi double(10, 2),
+    idPO_fk int(10) not null,
+    idStatus_fk int(10) not null
+);
+
+select * from produk;
