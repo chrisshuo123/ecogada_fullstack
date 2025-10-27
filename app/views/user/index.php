4c96c07496c4e0d98d7e2c6ad652397e44689126
chrisshuo123 <chr1z100@gmail.com>
2025-10-27 23:19:44 +0700

    in views/user, we create index.php as main user list page, and a login & register page each for sellers or merchants that own products (not users yet)

---
+++ app/views/user/index.php
@@ -0,0 +1,3 @@
+
+    <h1>User List Page</h1>
+    <p>Halo, nama saya <?= $data['namaDepan']; ?> <?= $data['namaBelakang']; ?> dengan username <?= $data['username']; ?>.  Saya memiliki password <?= $data['password']; ?> saya adalah seorang admin inti dari Ecogada Fullstack</p>
