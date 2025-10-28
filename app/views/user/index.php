3a498d9a6d3716a2bdb2ea125f6e720ec5849140
chrisshuo123 <chr1z100@gmail.com>
2025-10-29 03:11:16 +0700

    add container class and a class '.margin-top-body' to adjust the container distance within the top navbar

--- app/views/user/index.php
+++ app/views/user/index.php
@@ -1,3 +1,4 @@
-
+<div class="container margin-top-body">
     <h1>User List Page</h1>
     <p>Halo, nama saya <?= $data['namaDepan']; ?> <?= $data['namaBelakang']; ?> dengan username <?= $data['username']; ?>.  Saya memiliki password <?= $data['password']; ?> saya adalah seorang admin inti dari Ecogada Fullstack</p>
+</div>
