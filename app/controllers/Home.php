c40d741c094b6e6cce54e797449a53f47bd745e2
chrisshuo123 <chr1z100@gmail.com>
2025-10-29 16:07:15 +0700

    In controllers/Home, Add new param  in method index, and 3 new  array which are 'judul' (means title) [where we recently forgot to pass the header title], and 'nama' (means name), plus a  'julukan' (means nickname) returned to models/Home_model.

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -1,10 +1,13 @@
 <?php
 
 class Home extends Controller {
-    public function index() {
+    public function index($nama = 'Melani') {
         echo 'home/index';
-        $this->view('templates/header');
-        $this->view('home/index');
+        $data['judul'] = "Home";
+        $data['nama'] = $nama;
+        $data['julukan'] = $this->model('Home_model')->getHome();
+        $this->view('templates/header', $data);
+        $this->view('home/index', $data);
         $this->view('templates/footer');
     }
 }
