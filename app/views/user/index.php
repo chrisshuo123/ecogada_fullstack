fabedfd20ae1bdb6373dbca5a033635bc7b93767
chrisshuo123 <chr1z100@gmail.com>
2025-10-31 01:27:29 +0700

    add foreach tag that shows all lists of Databases from getAllUser() method in User model via User controllers

--- app/views/user/index.php
+++ app/views/user/index.php
@@ -1,4 +1,19 @@
 <div class="container margin-top-body">
     <h1>User List Page</h1>
     <p>Halo, nama saya <?= $data['namaDepan']; ?> <?= $data['namaBelakang']; ?> dengan username <?= $data['username']; ?>.  Saya memiliki password <?= $data['password']; ?> saya adalah seorang admin inti dari Ecogada Fullstack</p>
+    <div class="row">
+        <div class="col-6">
+            <h3>Daftar Mahasiswa</h3>
+            <?php foreach($data['user'] as $user): ?>
+                <ul>
+                    <li><?= $user['tglRegistrasi']; ?></li>
+                    <li><?= $user['namaDepan']; ?></li>
+                    <li><?= $user['namaBelakang']; ?></li>
+                    <li><?= $user['email']; ?></li>
+                    <li><?= $user['username']; ?></li>
+                    <li><?= $user['password']; ?></li>
+                </ul>
+            <?php endforeach; ?>
+        </div>
+    </div>
 </div>
