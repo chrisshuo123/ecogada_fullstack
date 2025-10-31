b2d95759ad77cbf46a9a4aca66ec5021ccb1e6b9
chrisshuo123 <chr1z100@gmail.com>
2025-11-01 01:09:45 +0700

    add new CRUD editor table view from table 'user' in app/views/user/index.php

--- app/views/user/index.php
+++ app/views/user/index.php
@@ -1,19 +1,32 @@
 <div class="container margin-top-body">
     <h1>User List Page</h1>
     <p>Halo, nama saya <?= $data['namaDepan']; ?> <?= $data['namaBelakang']; ?> dengan username <?= $data['username']; ?>.  Saya memiliki password <?= $data['password']; ?> saya adalah seorang admin inti dari Ecogada Fullstack</p>
-    <div class="row">
-        <div class="col-6">
-            <h3>Daftar Mahasiswa</h3>
+    <h2>List User Merchant EcoGada Goods</h2>
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Tanggal Registrasi</th>
+                <th scope="col">Nama</th>
+                <th scope="col">Username</th>
+                <th>Panel</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php $rowCount = 1; ?>
             <?php foreach($data['user'] as $user): ?>
-                <ul>
-                    <li><?= $user['tglRegistrasi']; ?></li>
-                    <li><?= $user['namaDepan']; ?></li>
-                    <li><?= $user['namaBelakang']; ?></li>
-                    <li><?= $user['email']; ?></li>
-                    <li><?= $user['username']; ?></li>
-                    <li><?= $user['password']; ?></li>
-                </ul>
+                <tr>
+                    <th scope="row"><?= $rowCount++ ?></th>
+                    <td scope="col"><?= $user['tglRegistrasi']; ?></td>
+                    <td scope="col"><?= $user['namaDepan']; ?> <?= $user['namaBelakang']; ?> </td>
+                    <td scope="col"><?= $user['username']; ?></td>
+                    <td>
+                        <button type="button" class="btn btn-primary">Tambah</button>
+                        <button type="button" class="btn btn-warning">Ubah</button>
+                        <button type="button" class="btn btn-danger">Hapus</button>
+                    </td>
+                </tr>
             <?php endforeach; ?>
-        </div>
-    </div>
+        </tbody>
+    </table>
 </div>
