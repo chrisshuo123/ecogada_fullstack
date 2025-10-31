22cee752383d3bb633fc0e99f770102b2d5cff05
chrisshuo123 <chr1z100@gmail.com>
2025-11-01 04:07:57 +0700

    add call link to core/Flasher.php in init.php

--- app/init.php
+++ app/init.php
@@ -5,3 +5,4 @@ require_once __DIR__ . "/config/config.php";
 require_once __DIR__ . "/core/App.php";
 require_once __DIR__ . "/core/Controller.php";
 require_once __DIR__ . "/core/Database.php";
+require_once __DIR__ . "/core/Flasher.php";
