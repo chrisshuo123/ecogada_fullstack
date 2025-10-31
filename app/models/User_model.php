82604fb4f02b61c3d206b08ef7857576a44f0974
chrisshuo123 <chr1z100@gmail.com>
2025-11-01 06:23:13 +0700

    add ubahDataUser method in User model that queries the Update Set, complete with error log debugger due to data-id in views/user/index.php that makes me confuse for not calling the actual idUser

--- app/models/User_model.php
+++ app/models/User_model.php
@@ -57,4 +57,50 @@ class User_model {
 
         return $this->db->rowCount();
     }
+
+    public function ubahDataUser($data) {
+
+        // DEBUG: Log semua data yang diterima
+        error_log("=== DEBUG UBAH DATA USER MERCHANT ===");
+        error_log("Data received:");
+        error_log("nama depan: " . $data['namaDepan']);
+        error_log("nama belakang: " . $data['namaBelakang']);
+        error_log("email: " . $data['email']);
+        error_log("username: " . $data['username']);
+        error_log("password: " . $data['password']);
+        error_log("id: " . $data['id']);
+        error_log("idUser: " . $data['idUser']);
+
+        $query = "UPDATE user SET
+                    namaDepan = :namaDepan,
+                    namaBelakang = :namaBelakang,
+                    email = :email,
+                    username = :username,
+                    password = :password
+                WHERE idUser = :id";
+
+        // DEBUG: Log binding values
+        error_log("Binding values:");
+        error_log(":namaDepan = " . $data['namaDepan']);
+        error_log(":namaBelakang = " . $data['namaBelakang']);
+        error_log(":email = " . $data['email']);
+        error_log(":username = " . $data['username']);
+        error_log(":password = " . $data['password']);
+        error_log(":id = " . $data['id']);
+        error_log(":idUser = " . $data['idUser']);
+
+        $this->db->query($query);
+        $this->db->bind(':namaDepan', $data['namaDepan']);
+        $this->db->bind(':namaBelakang', $data['namaBelakang']);
+        $this->db->bind(':email', $data['email']);
+        $this->db->bind(':username', $data['username']);
+        $this->db->bind(':password', $data['password']);
+        $this->db->bind(':id', $data['id']);
+    
+        $this->db->execute();
+
+        $rowCount = $this->db->rowCount();
+
+        return $rowCount;
+    }
 }
