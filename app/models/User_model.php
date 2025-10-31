acea92d5691a743aacbab30ef0f90e33476b61b2
chrisshuo123 <chr1z100@gmail.com>
2025-11-01 02:42:29 +0700

    add 3 new methods, which are getUserById method to retrieve details of a row, tambahDataUser method to insert new data row

--- app/models/User_model.php
+++ app/models/User_model.php
@@ -15,4 +15,27 @@ class User_model {
         $this->db->query('SELECT * FROM ' . $this->table);
         return $this->db->resultSet(); // Mengembalikan "semua" datanya
     }
+
+    public function getUserById($idUser) {
+        $this->db->query('SELECT * FROM ' . $this->table . ' WHERE idUser=:idUser');
+        $this->db->bind('idUser', $idUser);
+        return $this->db->single();
+    }
+
+    public function tambahDataUser($data) {
+        $query = "INSERT INTO user
+                VALUES
+                    ('', :tglRegistrasi, :namaDepan, :namaBelakang, :email, :username, :password)";
+        $this->db->query($query);
+        $this->db->bind(':tglRegistrasi', $data['tglRegistrasi']);
+        $this->db->bind(':namaDepan', $data['namaDepan']);
+        $this->db->bind(':namaBelakang', $data['namaBelakang']);
+        $this->db->bind(':email', $data['email']);
+        $this->db->bind(':username', $data['username']);
+        $this->db->bind(':password', $data['password']);
+
+        $this->db->execute();
+
+        return $this->db->rowCount();
+    }
 }
