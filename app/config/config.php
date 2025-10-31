3cd70e285508acac27051a404efcb87058aa6451
chrisshuo123 <chr1z100@gmail.com>
2025-11-01 01:07:34 +0700

    add define link for the SQL after adding the Database.php in core/Database.php

--- app/config/config.php
+++ app/config/config.php
@@ -2,3 +2,9 @@
 
 define('BASEURL', 'http://localhost/ecogada_fullstack/public');
 // define('BASEURL', 'http://localhost:3000/public'); // go to cd public directory
+
+define('DB_HOST', 'localhost');
+define('DB_PORT', '3307');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'ecogada');
