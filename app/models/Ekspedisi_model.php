d1094033bb54a02ed46a154d1e3f2fee87ce7cec
chrisshuo123 <chr1z100@gmail.com>
2025-11-03 04:17:33 +0700

    add new Ekspedisi Model, that contains Left Join query in getAllEkspedisiGrouped() method adding with array grouping due to m:n table contained in 3 table expeditions (table 'ekspedisi, jenis_ekspedisi, layanan_ekspedisi') to show Complete Expedition lists in views/ekspedisi/index page. For each table 'ekspedisi' we choose to read 'jenisEkspedisi' and 'deskripsi' from table jenis_ekspedisi, we return without Left Join. Due to complexity thinking, we add 3 debug methods called 'checkLayananEkspedisiTable, debugTableStructure, and checkEkspedisiTable'

---
+++ app/models/Ekspedisi_model.php
@@ -0,0 +1,144 @@
+<?php
+
+class Ekspedisi_model {
+    private $tableEkspedisi = 'ekspedisi';
+    private $tableJenisEkspedisi = 'jenis_ekspedisi';
+    private $tableLayananEkspedisi = 'layanan_ekspedisi';
+    private $db;
+
+    public function __construct() {
+        $this->db = new Database;
+    }
+
+    public function getNamaEkspedisi($idEkspedisi) {
+        $query = 'SELECT namaEkspedisi FROM ekspedisi WHERE idEkspedisi = :idEkspedisi';
+        $this->db->query($query);
+        $this->db->bind(":idEkspedisi", $idEkspedisi);
+        $result = $this->db->single();
+
+        if($result && isset($result['namaEkspedisi'])) {
+            return $result['namaEkspedisi']; // Return string nama saja
+        }
+        return "Ekspedisi tidak ditemukan di method getNamaEkspedisi";
+    }
+
+    public function getAllEkspedisiGrouped() { // Karna ini dari DB m:n
+        $query = '
+            SELECT
+                L.idLayananEkspedisi,
+                E.idEkspedisi,
+                E.namaEkspedisi,
+                J.jenisEkspedisi,
+                J.deskripsi
+            FROM
+                layanan_ekspedisi as L
+            LEFT JOIN
+                ekspedisi as E ON L.idEkspedisi_fkLayananEkspedisi = E.idEkspedisi
+            LEFT JOIN
+                jenis_ekspedisi as J ON L.idJenisEkspedisi_fkLayananEkspedisi = J.idJenisEkspedisi;
+            ';
+
+        $this->db->query($query);
+
+        try {
+            $rawData = $this->db->resultSet();
+        } catch (Exception $e) {
+            // Log error atau return array kosong
+            error_log("Database error: " . $e->getMessage());
+            return [];
+        }
+
+        // GROUPING DI MODEL - Struktur yang benar
+        // Group data by nama ekspedisi
+        $groupedData = [];
+        foreach($rawData as $item) {
+            $namaEkspedisi = $item['namaEkspedisi'];
+            if(!isset($groupedData[$namaEkspedisi])) {
+                $groupedData[$namaEkspedisi] = [
+                    'idEkspedisi' => $item['idEkspedisi'], // Simpan id ekspedisi disini
+                    'jenisLayanan' => [] // Array untuk jenis - jenis layanan
+                ];
+            }
+            $groupedData[$namaEkspedisi]['jenisLayanan'][] = $item['jenisEkspedisi'];
+        }
+
+        return $groupedData;
+    }
+
+    public function getJenisEkspedisi_By_IdEkspedisi($idEkspedisi) {
+        $query = 'SELECT J.jenisEkspedisi, J.deskripsi
+            FROM layanan_ekspedisi as L
+            LEFT JOIN 
+                jenis_ekspedisi as J ON L.idJenisEkspedisi_fkLayananEkspedisi = J.idJenisEkspedisi
+            WHERE
+                L.idEkspedisi_fkLayananEkspedisi = :idEkspedisi
+        ';
+
+        $this->db->query($query);
+        $this->db->bind(":idEkspedisi", $idEkspedisi);
+        $result = $this->db->resultSet();
+
+        // Debug hasil query
+        error_log("Query result for idEkspedisi $idEkspedisi: " . print_r($result, true));
+        
+        return $result;
+
+        // if($result && isset($result['idEkspedisi'])) {
+        //     return $result['idEkspedisi']; // Return string nama saja
+        // }
+        // return "idEkspedisi tidak ditemukan di method getJenisEkspedisi_By_IdEkspedisi";
+    }
+
+    // Sertakan method checkLayananEkspedisiTable memastikan idEkspedisi terbaca di di tabel layanan_ekspedisi:
+    // Bagian ini ditambahkan setelah mengdebug lewat method debugTableStructure dan checkEkspedisiTable
+    public function checkLayananEkspedisiTable($idEkspedisi) {
+        $query = "SELECT * FROM layanan_ekspedisi WHERE idEkspedisi_fkLayananEkspedisi = :idEkspedisi";
+        $this->db->query($query);
+        $this->db->bind(":idEkspedisi", $idEkspedisi);
+        $layananData = $this->db->resultSet();
+    
+        // Debug buat mencari seluruh data layanan ekspedisi dari idEkspedisi:
+        echo "<h4>Data di layanan ekspedisi: </h4>";
+        echo "<pre>";
+        print_r($layananData);
+        echo "</pre>";
+
+        // Cek tabel jenis_ekspedisi (tabel menampung seluruh layanan ekspedisi):
+        $query2 = "SELECT * FROM jenis_ekspedisi";
+        $this->db->query($query2);
+        $jenisData = $this->db->resultSet();
+
+        echo "<h4>Data di jenis_ekspedisi:</h4>";
+        echo "<pre>";
+        print_r($jenisData);
+        echo "</pre>";
+    }
+
+    // models/Ekspedisi_model.php
+    public function debugTableStructure() {
+        $query = "DESCRIBE ekspedisi";
+        $this->db->query($query);
+        return $this->db->resultSet();
+    }
+
+    // Cek Struktur Tabel Ekspedisi:
+    public function checkEkspedisiTable() {
+        $query = "SHOW COLUMNS FROM ekspedisi";
+        $this->db->query($query);
+        $columns = $this->db->resultSet();
+        
+        echo "<pre>Struktur Tabel Ekspedisi: ";
+        print_r($columns);
+        echo "</pre>";
+        
+        // Cek data yang ada
+        $query2 = "SELECT * FROM ekspedisi LIMIT 5";
+        $this->db->query($query2);
+        $data = $this->db->resultSet();
+        
+        echo "<pre>Data Ekspedisi: ";
+        print_r($data);
+        echo "</pre>";
+        exit;
+    }
+}
