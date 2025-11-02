25516488e85c26edd49013601ae17b43107c41cc
chrisshuo123 <chr1z100@gmail.com>
2025-11-03 04:23:18 +0700

    create a second detail page called views/ekspedisi/detail2 as front-end visualization to ease confuseness due to the detail page in views/ekspedisi/detail contain error recently

---
+++ app/views/ekspedisi/detail2.php
@@ -0,0 +1,35 @@
+<div class="container">
+    <h1>Detail Ekspedisi JNE</h1>
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Jenis Ekspedisi</th>
+                <th scope="col">Deskripsi</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th scope="row">1</th>
+                <td><strong>REG</td>
+                <td>Layanan Reguler</td>
+            </tr>
+            <tr>
+                <th scope="row">2</th>
+                <td><strong>OKE</td>
+                <td>Layanan Oke</td>
+            </tr>
+            <tr>
+                <th scope="row">3</th>
+                <td><strong>YES</td>
+                <td>Layanan Yes</td>
+            </tr>
+        </tbody>
+    </table>
+
+    <div class="mt-4">
+        <a href="<?= BASEURL; ?>/ekspedisi" class="btn btn-secondary">
+            ← Kembali ke Daftar Ekspedisi
+        </a>
+    </div>
+</div>
