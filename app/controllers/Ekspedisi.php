fdbf884afb82f75247705d458a64fbaebab9e372
chrisshuo123 <chr1z100@gmail.com>
2025-11-05 05:00:58 +0700

    disable error checking in method detail

--- app/controllers/Ekspedisi.php
+++ app/controllers/Ekspedisi.php
@@ -13,37 +13,83 @@ class Ekspedisi extends Controller {
     }
 
     public function detail($idEkspedisi) {
-        // Pastikan ID valid
-        // if (!is_numeric($idEkspedisi)) {
-        //     header('Location: ' . BASEURL . '/ekspedisi');
-        //     exit;
-        // }
+        // Karena didalam tag div class .container tidak menampilkan apa2, berarti ini masalah serius
+        // Aktifkan error reporting dgn Echo test 1 sampai 5 lalu flush
+        // error_reporting(E_ALL);
+        // ini_set('display_errors', 1);
+
+        // echo "TEST 1: Masuk method detail<br>";
+        // flush();
 
         $namaEkspedisi = $this->model('Ekspedisi_model')->getNamaEkspedisi($idEkspedisi);
+        // echo "TEST 2: Nama Ekspedisi: $namaEkspedisi<br>";
         // Get semua layanan (jenis_ekspedisi) yang terkait dengan ekspedisi ini
-        $data['ekspedisi'] = $this->model('Ekspedisi_model')->getJenisEkspedisi_By_IdEkspedisi($idEkspedisi);
-
-        echo "Nama Ekspedisi yang didapat: ";
-        var_dump($namaEkspedisi);
-        echo "IdEkspedisi yang didapat: ";
-        var_dump($idEkspedisi);
-        exit;
+        $data['jenisEkspedisi'] = $this->model('Ekspedisi_model')->getJenisEkspedisi_By_IdEkspedisi($idEkspedisi);
+        // echo "TEST 3: Data Jumlah Jenis Ekspedisi: " . count($data['jenisEkspedisi']) . "<br>";
+        // flush();
+        
+        // Debug menampilkan nama Ekspedisi (debug sebelumnya)
+        // echo "Nama Ekspedisi yang didapat: ";
+        // var_dump($namaEkspedisi);
+        // echo "IdEkspedisi yang didapat: ";
+        // var_dump($idEkspedisi);
+        // exit;
 
         // Jika tidak ada data, redirect ke list:
-        if (empty($data['ekspedisi'])) {
-            header('location: ' . BASEURL . '/ekspedisi');
-            exit;
-        }
+        // if (empty($data['ekspedisi'])) {
+        //     header('location: ' . BASEURL . '/ekspedisi');
+        //     echo "<script type='text/javascript'>alert('Data ekspedisi kosong');</script>";
+        //     exit;
+        // }
 
         $data['judul'] = "Detail " . $namaEkspedisi;
         // Judul pada halaman detail per ekspedisi (yg diatas untuk header):
         $data['judul_ekspedisi'] = $namaEkspedisi;
         
+        // echo "TEST 4: Sebelum load view<br>";
+        // flush();
 
+        // try {
+        //     echo "TEST 4.1: Sebelum templates/header";
+        //     $this->view('templates/header', $data);
+        //     echo "TEST 4.2: Sesudah templates/header, sebelum ekspedisi/detail";
+        //     $this->view('ekspedisi/detail', $data);
+        //     echo "TEST 4.3: Sesudah ekspedisi/detail, sebelum templates/footer";
+        //     $this->view('templates/footer');
+        //     echo "TEST 4.4: Sesudah templates/footer, dan diatas-atasnya";
+        // } catch(Exception $e) {
+        //     echo "Error in view: " . $e->getMessage() . "<br>";
+        // }
+        
         $this->view('templates/header', $data);
         $this->view('ekspedisi/detail', $data);
         $this->view('templates/footer');
+        // echo "TEST 5: Setelah load view<br>";
+        // flush();
     }
+
+    // Lakukan View Method langsung, ngedebug Test 1-5 Flush diatas ini
+    public function testView() {
+        echo "TEST VIEW METHOD START<br>";
+        
+        $data = ['judul' => 'Test Title'];
+        
+        try {
+            $this->view('templates/header', $data);
+            echo "HEADER SUCCESS<br>";
+            
+            $this->view('ekspedisi/detail', $data);
+            echo "DETAIL SUCCESS<br>";
+            
+            $this->view('templates/footer', $data);
+            echo "FOOTER SUCCESS<br>";
+        } catch (Exception $e) {
+            echo "VIEW ERROR: " . $e->getMessage() . "<br>";
+        }
+        
+        echo "TEST VIEW METHOD END<br>";
+        exit;
+    } // Jalankan pakai http://localhost/ecogada_fullstack/public/ekspedisi/testView
 
     // Khusus testing halaman Ekspedisi/detail saja
     public function detail2() {
